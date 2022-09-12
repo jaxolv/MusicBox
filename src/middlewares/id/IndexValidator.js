d1244d8a083c98af3f54c57bd51a2eca2856399f
jaxolv <jaxolv@gmail.com>
2022-09-11 22:35:51 -0300

    validação de id passados em params

---
+++ src/middlewares/id/IndexValidator.js
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+
+async function IndexValidator(req, res, next) {
+    const schema = yup.object().shape({
+        id: yup
+            .string()
+            .length(36)
+            .strict()
+            .required()
+            .typeError("Apenas texto.")
+        })
+
+    await schema.validate(req.params).catch(error => {
+        return res.status(400).json({
+            message: error.errors
+        })
+    })
+
+    next()
+}
+
+export default IndexValidator;
