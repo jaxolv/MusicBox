ebcdc7bd99cc6d22b0db8f8d94535fc90cdaea41
jaxolv <jaxolv@gmail.com>
2022-09-17 17:38:37 -0300

    Update de album

---
+++ src/app/controllers/album/UpdateAlbumController.js
@@ -0,0 +1,25 @@
+import UpdateAlbumService from "../../services/albums/UpdateAlbumService";
+
+export default class UpdateAlbumController {
+    constructor() { this.service = new UpdateAlbumService() }
+
+    async update(req, res) {
+        const { id } = req.params
+        const {
+            title,
+            release,
+            genre,
+            band_id
+        } = req.body
+
+        const resulte = await this.service.updateAlbum(
+            id,
+            title,
+            release,
+            genre,
+            band_id
+        )
+
+        return res.status(200).json(resulte)
+    }
+}
